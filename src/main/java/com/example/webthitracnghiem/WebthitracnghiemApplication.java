cf2a0ebf8684011031d47a3763f8bef68cf669f3
PhamVanPhuc - 2475 <vanphucpham2k4@gmail.com>
2026-03-24 22:52:30 +0700

    Khoi tao

---
+++ src/main/java/com/example/webthitracnghiem/WebthitracnghiemApplication.java
@@ -0,0 +1,13 @@
+package com.example.webthitracnghiem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebthitracnghiemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebthitracnghiemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webthitracnghiem/WebthitracnghiemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webthitracnghiem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebthitracnghiemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
