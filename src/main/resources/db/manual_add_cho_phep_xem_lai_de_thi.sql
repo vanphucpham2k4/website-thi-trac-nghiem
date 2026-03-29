fa572009e1664b58d397080d47cc6db47ceb3c41
PhamVanPhuc - 2475 <vanphucpham2k4@gmail.com>
2026-03-29 16:01:05 +0700

    xoa tab mon hoc cua sinh vien

--- src/main/resources/db/manual_add_cho_phep_xem_lai_de_thi.sql
+++
@@ -1,6 +0,0 @@
--- Chạy thủ công trên MySQL nếu bảng de_thi đã tồn tại mà chưa có cột (vd. môi trường không dùng spring.jpa.hibernate.ddl-auto=update).
--- Mặc định 1 = cho phép xem lại (tương thích dữ liệu cũ).
-
-ALTER TABLE de_thi
-    ADD COLUMN cho_phep_xem_lai TINYINT(1) NOT NULL DEFAULT 1
-    COMMENT 'Sinh viên được xem lại chi tiết từng câu sau khi nộp';
